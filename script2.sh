a46a1bbcca1d8fa42822c51ef8158d34dad5a5fb
dee-mee <derekmuriuki2@gmail.com>
2024-04-01 16:21:46 +0300

    my second bash script

---
+++ script2.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#this script will show your username
+#and the currently running processes
+echo ""
+echo "Hello $USER"
+echo ""
+echo "Welcome $USER. I will be telling you about the currently running processes."
+echo ""
+echo "Running processes..."
+ps
