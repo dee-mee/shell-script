54b8909e3b9eac37945e8831edc18c1f2d7a3103
dee-mee <derekmuriuki2@gmail.com>
2024-06-01 14:56:01 +0300

    new file:   weather.sh

---
+++ weather.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# weather.sh
+
+program=Weather
+year=2024
+developer=dee-mee
+
+case $1 in
+-h | --help)
+	echo "$program $version"
+	echo "Copyright $year $developer. All rights reserved."
+	echo
+	echo "Usage: weather [options]"
+	echo "Option          Long Option             Description"
+	echo "-h              --help                  Show the help screen"
+	echo "-l [location]   --location [location]   Specifies the location"
+	;;
+-l | --location)
+	curl https://wttr.in/$2
+	;;
+*)
+	curl https://wttr.in
+	;;
+esac
