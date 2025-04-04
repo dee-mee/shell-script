a1e4b8c57215ac96883a381800cb01922c421ba1
dee-mee <derekmuriuki2@gmail.com>
2025-04-04 13:52:21 +0300

    removed outdated installs

--- install.sh
+++ install.sh
@@ -90,13 +90,8 @@ echo "========================================="
 echo "						"
 sudo apt install curl
 
-sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
-
-echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
-
 sudo apt update
 
-sudo apt install brave-browser
 echo "The pc is going to reboot in 5 secs to stop press control + c"
 
 sleep 5
